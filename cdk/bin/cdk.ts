5de35d8faaea03d561f1996b3e32df486ea61d74
akshayp-vwits <akshay.patil1@volkswagen.co.in>
2021-06-16 11:04:23 +0530

    Build all at once

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -23,23 +23,12 @@ const envConfigs = {
     }
 }
 
+const dev  = { account: '221090739390', region: 'eu-west-1' };
+
 //const stage = app.node.tryGetContext('stage')
 // @ts-ignore
 //const acc_no = envConfigs[stage].account_id
 // @ts-ignore
 //const region1 = envConfigs[stage].region
-new CdkStack(app, 'CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+new CdkStack(app, 'dev', { env: dev });
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
